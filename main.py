cdc3ffd7433f9d146f3535cede55e4c07e0326a2
Charly Mercury <carlos.mercury92@gmail.com>
2022-07-26 01:12:52 -0500

    First dev commit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 
+
+users = []
+
 app = FastAPI()
 
 
@@ -8,13 +11,24 @@ def home():
     return "Welcome To Charly's Mercury BlogSpot"
 
 
-@app.get(path = "/home")
-def home():
+@app.get(
+    path = "/users"
+    )
+def users_list():
+    return users
+
+@app.post(path = "/users")
+def create_user():
     return {"hellow":"CharlyMercury"}
 
 
-@app.get(
-    path = "/amoablanquita"
-)
-def amoablanquita():
-    return {"holis":"Blaquita"}
+@app.put(path = "/users/{user_id}")
+def update():
+    return {"hellow":"CharlyMercury"}
+
+
+@app.delete(path = "/users/{user_id}")
+def delete():
+    return {"hellow":"CharlyMercury"}
+
+
